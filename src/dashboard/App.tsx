2211c15bed9cee3832eeae83240644233d89485b
03walijamal-debug <03walijamal@gmail.com>
2026-09-04 14:57:51 +0000

    Add ChatPage to dashboard navigation

--- src/dashboard/App.tsx
+++ src/dashboard/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import DashboardLayout from "./DashboardLayout";
 import DashboardOverview from "./DashboardOverview";
 import AgentsPage from "./AgentsPage";
+import ChatPage from "./ChatPage";
 import KnowledgeBasePage from "./KnowledgeBasePage";
 import AnalyticsPage from "./AnalyticsPage";
 import SettingsPage from "./SettingsPage";
@@ -13,6 +14,8 @@ export default function App() {
     switch (currentPage) {
       case "agents":
         return <AgentsPage />;
+      case "chat":
+        return <ChatPage />;
       case "knowledge-base":
         return <KnowledgeBasePage />;
       case "analytics":
@@ -26,8 +29,8 @@ export default function App() {
   };
 
   return (
-    <DashboardLayout 
-      currentPage={currentPage} 
+    <DashboardLayout
+      currentPage={currentPage}
       onNavigate={setCurrentPage}
     >
       {renderPage()}
